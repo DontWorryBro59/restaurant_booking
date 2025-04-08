8d40978ccc34861738aba956655b1e65c5d90587
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-04-08 23:48:44 +0500

    Added: lifetime and mod verification logic. Cleaning and creating tables during development

--- app/main.py
+++ app/main.py
@@ -1,13 +1,38 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
-from app.routers.tables_router import tables_router
+from app.core.config import settings
+from app.core.custom_logger import get_logger
+from app.database.database_helper import db_helper
 from app.routers.reserv_router import reservation_router
+from app.routers.tables_router import tables_router
+
+logger = get_logger(__name__)
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("🚀 Application startup")
+    # Выполняем дополнительные действия в зависимости от режима работы
+    await check_app_mode()
+    yield
+    logger.info("🛑 Application shutdown")
 
-# Подключение роутеров
+
+async def check_app_mode():
+    if settings.MODE == "DEV":
+        logger.info("💻💻💻💻 Dev mode enabled 💻💻💻💻")
+        logger.info("Dropping database tables")
+        await db_helper._drop_all()
+        logger.info("Creating database tables")
+        await db_helper._create_all()
+
+
+app = FastAPI(lifespan=lifespan)
+
+# Добавляем роутеры
 app.include_router(tables_router)
 app.include_router(reservation_router)
 

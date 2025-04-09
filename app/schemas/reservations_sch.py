fa5c1a09764869c8b0df4755603d286cb7d9a12a
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-04-09 15:46:58 +0500

    Remove mistake from reservations_sch (validate_reservation_time)

--- app/schemas/reservations_sch.py
+++ app/schemas/reservations_sch.py
@@ -1,7 +1,12 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import Optional
 
+from fastapi import HTTPException, status
 from pydantic import BaseModel, Field, field_validator, ConfigDict
+
+from app.core.custom_logger import get_logger
+
+logger = get_logger(__name__)
 
 
 class ReservationBase(BaseModel):
@@ -11,19 +16,33 @@ class ReservationBase(BaseModel):
     duration_minutes: int = Field(..., ge=15, le=300)
 
     @field_validator("reservation_time")
-    def validate_reservation_time(cls, value):
-        if value < datetime.now():
-            raise ValueError("Reservation time must be in the future.")
+    def validate_reservation_time(cls, value: datetime) -> datetime:
+        # Проверка наличия информации о временной зоне
+        if value.tzinfo is None or value.tzinfo.utcoffset(value) is None:
+            message = "Время бронирования должно содержать информацию о временной зоне."
+            logger.error(message)
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=message)
+
+        # Получение текущего времени в UTC
+        current_time = datetime.now(timezone.utc)
+
+        # Проверка, что время бронирования в будущем
+        if value <= current_time:
+            message = "Время бронирования должно быть в будущем."
+            logger.error(message)
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=message)
         return value
 
 
 class ReservationCreate(ReservationBase):
     pass
 
+
 class ReservationRead(ReservationBase):
     id: int
 
     model_config = ConfigDict(from_attributes=True)
+
 
 class ReservationUpdate(BaseModel):
     customer_name: Optional[str] = None
